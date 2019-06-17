98f8cae326cdc3c5f2e590240c456244fffab43d
Sergio Caballero <scaballero@yomyer.com>
2019-06-17 17:49:46 +0200

    Initial Commit

---
+++ src/ios/PaypalBraintree.m
@@ -0,0 +1,28 @@
+/********* PaypalBraintree.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface PaypalBraintree : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation PaypalBraintree
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
